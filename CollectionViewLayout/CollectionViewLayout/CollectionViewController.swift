fb5bc199901036a7995c498707b9f7c602d83b10
WilliamMaybe <271138178@qq.com>
2018-08-27 01:21:01 +0800

    [ADD]Initial demo

---
+++ CollectionViewLayout/CollectionViewLayout/CollectionViewController.swift
@@ -0,0 +1,105 @@
+//
+//  CollectionViewController.swift
+//  CollectionViewLayout
+//
+//  Created by maybe on 2018/8/26.
+//  Copyright © 2018年 Maybe Zh. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class EmojiCell: UICollectionViewCell {
+    @IBOutlet weak var emojiLabel: UILabel!
+}
+
+class CollectionViewController: UICollectionViewController {
+    
+    var emojis: [[String]] = []
+    
+    override func viewDidLoad() {
+        refresh()
+        super.viewDidLoad()
+    }
+
+    @IBAction func addEmoji(_ sender: Any) {
+        let section = Int(arc4random_uniform(UInt32(emojis.count)))
+        let row = Int(arc4random_uniform(UInt32(emojis[section].count)))
+        emojis[section].insert("😄", at: row)
+        collectionView?.insertItems(at: [IndexPath(row: row, section: section)])
+    }
+    @IBAction func deleteEmoji(_ sender: Any) {
+        let section = Int(arc4random_uniform(UInt32(emojis.count)))
+        let row = Int(arc4random_uniform(UInt32(emojis[section].count)))
+        emojis[section].remove(at: row)
+        collectionView?.deleteItems(at: [IndexPath(row: row, section: section)])
+    }
+    
+    func refresh()  {
+        let ids = 1...2
+        let emoticons = (0x1F600...0x1F647).compactMap { UnicodeScalar($0).map(String.init) }
+        let splitedCount = Int((Double(emoticons.count) / Double(ids.count)).rounded(.up))
+        
+        emojis = ids.enumerated().map { offset, model in
+            let start = offset * splitedCount
+            let end = min(start + splitedCount, emoticons.endIndex)
+            let emoticons = emoticons[start..<end]
+            return Array(emoticons)
+        }
+    }
+    
+    
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return emojis.count
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return emojis[section].count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! EmojiCell
+        cell.emojiLabel.text = emojis[indexPath.section][indexPath.row]
+        cell.backgroundColor = UIColor(red: CGFloat(arc4random_uniform(255)) / 255.0,
+                                       green: CGFloat(arc4random_uniform(255)) / 255.0,
+                                       blue: CGFloat(arc4random_uniform(255)) / 255.0,
+                                       alpha: 1)
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
---
+++ CollectionViewLayout/CollectionViewLayout/CollectionViewSectionColorFlowLayout.swift
@@ -0,0 +1,106 @@
+//
+//  CollectionViewSectionColorFlowLayout.swift
+//  CollectionViewLayout
+//
+//  Created by maybe on 2018/8/26.
+//  Copyright © 2018年 Maybe Zh. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewSectionColorFlowLayout: UICollectionViewFlowLayout {
+    
+    override init() {
+        super.init()
+        register(CollectionViewSectionColorView.self, forDecorationViewOfKind: CollectionViewSectionColorView.identifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        register(CollectionViewSectionColorView.self, forDecorationViewOfKind: CollectionViewSectionColorView.identifier)
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let tmp = super.layoutAttributesForElements(in: rect)
+        
+        guard var attributes = tmp else {
+            return tmp
+        }
+        
+        
+        guard let numberOfSection = collectionView?.numberOfSections  else {
+            return attributes
+        }
+        #if TEST
+        for section in 0..<numberOfSection {
+            guard let numberOfItems = collectionView?.numberOfItems(inSection: section) else {
+                continue;
+            }
+            
+            if numberOfItems == 0 {
+                continue
+            }
+            
+            let start = layoutAttributesForItem(at: IndexPath(row: 0, section: section))!
+            let end = layoutAttributesForItem(at: IndexPath(row: numberOfItems - 1, section: section))!
+            
+            guard let decoration = layoutAttributesForDecorationView(ofKind: CollectionViewSectionColorView.identifier, at: start.indexPath) else {
+                continue
+            }
+
+//            let decoration = UICollectionViewLayoutAttributes(forDecorationViewOfKind: CollectionViewSectionColorView.identifier, with: start.indexPath)
+//            decoration.zIndex = -1
+            
+            decoration.frame = CGRect(x: 0, y: start.frame.minY - sectionInset.top, width: collectionView?.frame.width ?? 0, height: end.frame.maxY - start.frame.minY + sectionInset.top + sectionInset.bottom )
+            attributes.append(decoration)
+        }
+        #endif
+        var sections: Set<Int> = []
+        for attribute in attributes {
+            if attribute.representedElementCategory == .cell {
+                sections.insert(attribute.indexPath.section)
+            }
+        }
+
+        for section in sections.sorted(by: <) {
+            let endRow = collectionView?.numberOfItems(inSection: section) ?? 0
+            guard endRow > 0 else {
+                continue
+            }
+
+            let start = layoutAttributesForItem(at: IndexPath(row: 0, section: section))!
+            let end = layoutAttributesForItem(at: IndexPath(row: endRow - 1, section: section))!
+
+            guard let decoration = layoutAttributesForDecorationView(ofKind: CollectionViewSectionColorView.identifier, at: start.indexPath) else {
+                continue
+            }
+
+            decoration.frame = CGRect(x: 0, y: start.frame.minY - sectionInset.top, width: collectionView?.frame.width ?? 0, height: end.frame.maxY - start.frame.minY + sectionInset.top + sectionInset.bottom )
+            attributes.append(decoration)
+        }
+        
+        return attributes
+    }
+    
+    override func layoutAttributesForDecorationView(ofKind elementKind: String, at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        let attribute = UICollectionViewLayoutAttributes(forDecorationViewOfKind: elementKind, with: indexPath)
+        attribute.zIndex = -1
+        return attribute
+    }
+}
+
+
+private class CollectionViewSectionColorView: UICollectionReusableView {
+    fileprivate static let identifier = String(describing: self)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor(red: CGFloat(arc4random_uniform(255)) / 255.0,
+                                  green: CGFloat(arc4random_uniform(255)) / 255.0,
+                                  blue: CGFloat(arc4random_uniform(255)) / 255.0,
+                                  alpha: 1)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
